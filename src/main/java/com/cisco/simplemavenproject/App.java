1580cdfc63d0b5ebd9b191e86b026a9767cedc58
sonam-niit <sonam.gravity@gmail.com>
2024-02-29 11:46:42 +0530

    My Maven Project version1

---
+++ src/main/java/com/cisco/simplemavenproject/App.java
@@ -0,0 +1,10 @@
+package com.cisco.simplemavenproject;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the World of Jenkins!" );
+        System.out.println("Hello World from My simple Project");
+    }
+}
